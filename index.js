51f7fc41a9c5692a478cbac176e0f6f7fa2cf012
janobnotiq <janobnotiq@gmail.com>
2024-12-10 11:41:56 +0500

    some js codes

---
+++ index.js
@@ -0,0 +1,63 @@
+// // console.log(`Assalomu alaykum!`)
+
+// // window.alert(`Tabriklaymiz, siz bugun kutibxonaga keldingiz!`)
+
+// // document.getElementById("hello").textContent = "Hello";
+// // document.getElementById("test").textContent = "test";
+
+// // let me = "go";
+// // let something = typeof me;
+
+// // console.log(`Let me ${me}`);
+// // console.log(`You can ${me}`);
+// // console.log(typeof me);
+// // console.log(typeof something);
+
+// // let firstName;
+
+// // firstName = window.prompt(`Enter your name:`)
+
+// // console.log(`Your name is ${firstName}`)
+
+// // let ad;
+// // ad = window.prompt(`Nimadir yoz`);
+
+// // document.getElementById("banner").textContent = ad;
+
+// // document.getElementById("go").onclick = function(){
+// //     ad = document.getElementById("yourad").value
+// //     document.getElementById("banner").textContent = ad;
+// // }
+
+// // const Amount = document.getElementById("amount");
+// // console.log(Amount)
+// const checked = document.getElementById("checked");
+// const uzcard = document.getElementById("Uzcard");
+// const humo = document.getElementById("Humo");
+// const visa = document.getElementById("Visa");
+// let result = document.getElementById("")
+// document.getElementById("pay").onclick = function(){
+//     if (uzcard.checked){
+//         window.alert("you are paying using uzcard")
+//     } else if (humo.checked){
+//         window.alert("you are paying using humo")
+//     } else if (visa.checked){
+//         window.alert("you are paying using visa")
+//     } else{
+//         window.alert("Other payment method")
+//     }
+// }
+
+// // let tax;
+
+// // pay.addEventListener("click",evt=>{
+// //     evt.preventDefault()
+// //     console.log(Amount.value)
+// // })
+
+// let age = 16;
+
+// let message = age >= 16 ? "You have a passport" : "You don't have a passport"
+
+// window.alert(message);
+
