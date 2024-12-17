eeb6e7e00fb549630eaa8697c785516133d433e4
janobnotiq <janobnotiq@gmail.com>
2024-12-17 11:58:56 +0500

    reverse string in js

--- index.js
+++ index.js
@@ -43,33 +43,48 @@
 
 // ---------------------------------------------------------------------------
 
-let secret = Math.floor((Math.random()*100));
-let attempts = 0;
-let running = true;
-let option;
+// let secret = Math.floor((Math.random()*100));
+// let attempts = 0;
+// let running = true;
+// let option;
 
-while(running){
-    option = window.prompt(`1 dan 100 gacha bo'lgan qaysidir son bu, qani top-chi`)
-    option = Number(option)
+// while(running){
+//     option = window.prompt(`1 dan 100 gacha bo'lgan qaysidir son bu, qani top-chi`)
+//     option = Number(option)
 
-    if (isNaN(option)){
-        window.alert(`Son kiriting`)
-    } else if (option < 1 || option > 100){
-        window.alert(`1 dan 100 gacha ukaxon!`)
-    } else if (option > secret) {
-        window.alert("Bundan kichik")
-        attempts++;
-    } else if (option < secret){
-        window.alert(`Bundan katta`)
-        attempts++
-    } else{
-        console.log(option)
+//     if (isNaN(option)){
+//         window.alert(`Son kiriting`)
+//     } else if (option < 1 || option > 100){
+//         window.alert(`1 dan 100 gacha ukaxon!`)
+//     } else if (option > secret) {
+//         window.alert("Bundan kichik")
+//         attempts++;
+//     } else if (option < secret){
+//         window.alert(`Bundan katta`)
+//         attempts++
+//     } else{
+//         console.log(option)
+//     }
+
+//     if (option === secret){
+//         attempts++;
+//         window.alert(`Siz ${attempts} urunishda bu son ${secret} ekanini topdingiz` )
+//         running = false;
+//         document.getElementById("res").textContent = "Qale"
+//     }
+// }
+// ------------------------------------------------------------------------------------
+
+let a = "Wertical"
+
+function reverse_string(matn){
+    let new_one = "";
+    if (matn.length > 0){
+        for (i=matn.length-1;i>=0;i--){
+            new_one = new_one + matn.charAt(i)
+        }
     }
-
-    if (option === secret){
-        attempts++;
-        window.alert(`Siz ${attempts} urunishda bu son ${secret} ekanini topdingiz` )
-        running = false;
-        document.getElementById("res").textContent = "Qale"
-    }
+    return new_one;
 }
+console.log(reverse_string(a))
+console.log(a)
