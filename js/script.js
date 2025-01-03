b3d88494fe8ab3b3a2b392efd0c8a0367a10e13e
janobnotiq <janobnotiq@gmail.com>
2025-01-03 12:27:18 +0500

    something to be pushed

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const Person = {
+    firstName: 'Ulugbek',
+    lastName: 'Tojimakhammatov',
+    age: 23,
+    job: 'Software engineer',
+    specialWord: function () {
+        console.log("O'ynab qo'yay!")
+    }
+}
+
+console.log(Person.age)
+Person.specialWord()
+console.log(Person)
